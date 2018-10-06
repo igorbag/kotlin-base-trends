fbbd5cf0ae04772fdcd253b3209402deefab0f05
Igor Rotondo Bagliotti <igor.bagliotti@gmail.com>
2018-10-06 18:35:16 -0300

    Adicionando Material Design Components V2 para a stack

--- app/src/main/java/com/example/igorrotondobagliot/kotlinbasetrends/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/igorrotondobagliot/kotlinbasetrends/ui/main/MainActivity.kt
@@ -5,9 +5,7 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import com.example.igorrotondobagliot.kotlinbasetrends.R
 import com.example.igorrotondobagliot.kotlinbasetrends.di.component.DaggerActivityComponent
-import com.example.igorrotondobagliot.kotlinbasetrends.di.component.DaggerFragmentComponent
 import com.example.igorrotondobagliot.kotlinbasetrends.di.module.ActivityModule
-import com.example.igorrotondobagliot.kotlinbasetrends.di.module.FragmentModule
 import com.example.igorrotondobagliot.kotlinbasetrends.models.Album
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
